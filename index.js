f188f808e2d5b0c8136d777c3e3af20c97b2eb45
Ervin <75392265+FrequentErvin@users.noreply.github.com>
2022-02-04 12:08:13 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+document.getElementById("GoHome").onclick=function(){
+    location.href="index.html";
+  };
+  // document.getElementById("checklist").onclick=function(){
+  //   location.href="MOchecklist.html";
+  // };
+  document.getElementById("blog").onclick=function(){
+    location.href="blog.html";
+  };
+  document.getElementById("project").onclick=function(){
+    location.href="projects.html";
+  };
+  document.getElementById("tofacts").onclick=function(){
+    location.href="facts.html";
+  };
