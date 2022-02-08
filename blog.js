284e5043994d13272289515a31140e3951423f7d
FrequentErvin <75392265+FrequentErvin@users.noreply.github.com>
2022-02-08 16:36:04 +0100

    Migrating blog to separate pages

--- blog.js
+++ blog.js
@@ -23,3 +23,23 @@ function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+var rotation = 0;
+function create() {
+  var b = document.querySelector('#blob');
+  var r1 = 30
+  var r2 = 50
+  var r3 = 60
+  var r4 = 30
+  var r11 = remain(r1);
+  var r22 = remain(r2);
+  var r33 = remain(r3);
+  var r44 = remain(r4);
+  var coordinates = `${r1}% ${r11}% ${r22}% ${r2}% / ${r3}% ${r4}% ${r44}% ${r33}%`;
+  b.style['border-radius'] = coordinates;
+  b.style['transform'] = 'rotate(' + rotation + 'deg)';
+  image.style.setProperty('--r', -rotation + 'deg');
+};
+function remain(n) {
+  return 100 - n;
+}
+create();
