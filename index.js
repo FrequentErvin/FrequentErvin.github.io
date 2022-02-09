535cafcb50e189c6af4fe010f956bda557228d54
FrequentErvin <75392265+FrequentErvin@users.noreply.github.com>
2022-02-09 18:39:17 +0100

    Separating blog posts into distinct page files

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 document.getElementById("GoHome").onclick=function(){
-    location.href="index.html";
+    location.href="/index.html";
   };
-  // document.getElementById("checklist").onclick=function(){
-  //   location.href="MOchecklist.html";
-  // };
   document.getElementById("blog").onclick=function(){
-    location.href="blog.html";
+    location.href="/blog.html";
   };
   document.getElementById("project").onclick=function(){
-    location.href="projects.html";
+    location.href="/projects.html";
   };
   document.getElementById("tofacts").onclick=function(){
-    location.href="facts.html";
+    location.href="/facts.html";
   };
   document.getElementById("reflectimo").onclick=function(){
     location.href="/blogentries/reflectonimo.html";
