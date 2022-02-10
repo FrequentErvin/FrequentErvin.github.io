869e25a73d9f0772c347724d3f0b828b89e7e7c0
FrequentErvin <75392265+FrequentErvin@users.noreply.github.com>
2022-02-10 12:29:15 +0100

    Fixing up continuity blog post

--- blog.js
+++ blog.js
@@ -1,3 +1,7 @@
+document.getElementById("intermediate").onclick=function(){
+  location.href="/blogentries/discretecont.html";
+};
+
 //Get the button
 let mybutton = document.getElementById("scroll");
 
@@ -24,44 +28,4 @@ function backToTop() {
   document.documentElement.scrollTop = 0;
 }
 
-//Blob things: 
 
-var rotation = 0;
-function create() {
-  var b = document.querySelector('#blob');
-  var r1 = 30
-  var r2 = 50
-  var r3 = 60
-  var r4 = 30
-  var r11 = remain(r1);
-  var r22 = remain(r2);
-  var r33 = remain(r3);
-  var r44 = remain(r4);
-  var coordinates = `${r1}% ${r11}% ${r22}% ${r2}% / ${r3}% ${r4}% ${r44}% ${r33}%`;
-  b.style['border-radius'] = coordinates;
-  b.style['transform'] = 'rotate(' + rotation + 'deg)';
-  image.style.setProperty('--r', -rotation + 'deg');
-};
-function remain(n) {
-  return 100 - n;
-}
-function create2() {
-  var b = document.querySelector('#blob2');
-  var r1 = 70
-  var r2 = 70
-  var r3 = 50
-  var r4 = 60
-  var r11 = remain(r1);
-  var r22 = remain(r2);
-  var r33 = remain(r3);
-  var r44 = remain(r4);
-  var coordinates = `${r1}% ${r11}% ${r22}% ${r2}% / ${r3}% ${r4}% ${r44}% ${r33}%`;
-  b.style['border-radius'] = coordinates;
-  b.style['transform'] = 'rotate(' + rotation + 'deg)';
-  image.style.setProperty('--r', -rotation + 'deg');
-};
-function remain(n) {
-  return 100 - n;
-}
-create();
-create2();
